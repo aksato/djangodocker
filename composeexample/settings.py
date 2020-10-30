579d2482ddd865076a335ee277645d8de421443f
Andre Sato <andre.kubagawa@usp.br>
2020-10-30 10:18:19 -0300

    Removido container Postgres

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cr8_ns=l4er#n#_cu(2tbri@ck6bit$#fy@1wcma1%&y823&k*'
+SECRET_KEY = '#j+t6s3w9d5pxjo_4ymdzy)=e02fx!97i_qhwqxz$#v_vbahw#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,15 +75,10 @@ WSGI_APPLICATION = 'composeexample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
