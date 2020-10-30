46dd5d30c95fe2a71589ef8d35d2e263fad5f16e
Andre Sato <andre.kubagawa@usp.br>
2020-10-30 14:36:30 -0300

    Incluido suporte Webpack para React sem JSX

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,2 +1,15 @@
-const btn = document.getElementById('click')
-btn.addEventListener('click', () => alert('You clicked the button!'))
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Define the React app
+const App = () => {
+  const [count, setCount] = React.useState(0)
+  const onClick = () => setCount(c => c + 1)
+  return React.createElement('div', null,
+    React.createElement('h1', null, 'The count is ' + count),
+    React.createElement('button', { onClick: onClick }, 'Count'),
+  )
+}
+// Mount the app to the mount point.
+const root = document.getElementById('app')
+ReactDOM.render(React.createElement(App, null, null), root)
